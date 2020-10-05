532051dd0442e571cbdfef9627bbfb28e4dfcd50
DDDDanny <707956456@qq.com>
2020-10-06 03:15:45 +0800

    feat: 优化用户登出逻辑

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -75,20 +75,16 @@ const actions = {
     // user logout
     logout({ commit, state, dispatch }) {
         return new Promise((resolve, reject) => {
-            logout(state.token).then(() => {
+            try {
                 commit('SET_TOKEN', '')
                 commit('SET_ROLES', [])
                 removeToken()
                 resetRouter()
-
-                // reset visited views and cached views
-                // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
                 dispatch('tagsView/delAllViews', null, { root: true })
-
                 resolve()
-            }).catch(error => {
-                reject(error)
-            })
+            } catch (e) {
+                reject(e)
+            }
         })
     },
 
